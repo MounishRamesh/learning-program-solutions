1053df36c26263f640aa3df0237077222722b09b
Mounish R <mounishr6680@gmail.com>
2025-06-25 19:40:45 +0530

    Update CalculatorTest.java

--- Week_2_TDD_using_JUnit5_and_Mockito/1_Setting_Up_JUnit/Code/CalculatorTest.java
+++ Week_2_TDD_using_JUnit5_and_Mockito/1_Setting_Up_JUnit/Code/CalculatorTest.java
@@ -1,1 +1,13 @@
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5, 3);
+        assertEquals(8, result, "Expected 5 + 3 to be 8");
+    }
+}
