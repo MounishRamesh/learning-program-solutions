bdd71a570747a907e7b49eacc64cedc083958ca6
Mounish R <mounishr6680@gmail.com>
2025-06-27 08:43:41 +0530

    Update StringUtils.java

--- Week_2_TDD_using_JUnit5_and_Mockito/3_AAA_Pattern_and_Teardown/Code/StringUtils.java
+++ Week_2_TDD_using_JUnit5_and_Mockito/3_AAA_Pattern_and_Teardown/Code/StringUtils.java
@@ -1,1 +1,10 @@
 
+public class StringUtils {
+    public String concatenate(String a, String b) {
+        return a + b;
+    }
+
+    public String toUpperCase(String input) {
+        return input.toUpperCase();
+    }
+}
