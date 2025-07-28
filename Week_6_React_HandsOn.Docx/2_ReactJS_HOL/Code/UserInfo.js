ea4b734ef1b3fa479db327f0a0d17c783a0cb710
Mounish R <mounishr6680@gmail.com>
2025-07-28 15:34:23 +0530

    Create UserInfo.js

---
+++ Week_6_React_HandsOn.Docx/2_ReactJS_HOL/Code/UserInfo.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+function UserInfo() {
+  const name = "Monish";
+  return (
+    <div>
+      <h2>Welcome, {name}</h2>
+    </div>
+  );
+}
+
+export default UserInfo;
