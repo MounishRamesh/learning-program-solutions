152900b3b33e0427e0887fcaf0ca1b2039e2ae9a
Mounish R <mounishr6680@gmail.com>
2025-08-05 12:30:07 +0530

    Delete Week_4_Spring_REST_using_Spring_Boot_3_HandsOn.docx/Exercise_5_Country_By_Code directory

--- Week_4_Spring_REST_using_Spring_Boot_3_HandsOn.docx/Exercise_5_Country_By_Code/Code/CountryController.java
+++
@@ -1,9 +0,0 @@
-
-
-@GetMapping("/countries/{code}")
-public Country getCountryByCode(@PathVariable String code) {
-    return getAllCountries().stream()
-            .filter(c -> c.getCode().equalsIgnoreCase(code))
-            .findFirst()
-            .orElseThrow(() -> new RuntimeException("Country not found"));
-}
